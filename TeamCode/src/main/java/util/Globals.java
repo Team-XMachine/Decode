5bd5055aa139db1b564ccdae98cebdbd275c7e01
skywalker <lipeeskywalker@gmail.com>
2025-10-20 14:18:49 -0300

    feat: season global values

--- TeamCode/src/main/java/util/Globals.java
+++ TeamCode/src/main/java/util/Globals.java
@@ -5,5 +5,9 @@ import com.acmerobotics.dashboard.config.Config;
 @Config
 public class Globals {
 
-    public static double MARKER_MAX_DISTANCE = 5;
+    public static double TRANSFER_POWER = 1;
+    public static double CHECKER_DISTANCE = 8;
+    public static double ITK_DISTANCE = 10;
+    public static double INTAKE_POWER = -1;
+    public static double SHOOTER_POWER = -1;
 }
