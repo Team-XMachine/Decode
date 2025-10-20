ccca292f1c1d698bddfc49a7af51b6bcd87bb959
skywalker <lipeeskywalker@gmail.com>
2025-10-20 14:19:57 -0300

    feat: added robot subsystems

---
+++ TeamCode/src/main/java/systems/IntakeSystem.java
@@ -0,0 +1,12 @@
+package systems;
+
+import enums.IntakeStates;
+
+public class IntakeSystem {
+    public static IntakeStates PS, CS = IntakeStates.INITIALIZE;
+
+
+    public void update() {
+
+    }
+}
